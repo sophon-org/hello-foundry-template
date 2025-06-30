b8a7aa128265eda901a85d3514f2bddfa5cf06e1
VargaElod23 <vargaelod23@gmail.com>
2025-06-30 14:04:32 +0200

    clear example

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -3,23 +3,27 @@ pragma solidity ^0.8.20;
 
 import {Script} from "forge-std/Script.sol";
 import {MockTokenWithPermit} from "../src/MockTokenWithPermit.sol";
-import {console} from "forge-std/console.sol";
 
 contract DeployMockTokenWithPermit is Script {
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("WALLET_PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
-        
+       address paymaster = address(
+            0x98546B226dbbA8230cf620635a1e4ab01F6A99B2
+        );
+        bytes memory paymaster_encoded_input = abi.encodeWithSelector(
+            bytes4(keccak256("general(bytes)")),
+            bytes("0x")
+        );
+        (bool success, ) = address(vm).call(
+            abi.encodeWithSignature(
+                "zkUsePaymaster(address,bytes)",
+                paymaster,
+                paymaster_encoded_input
+            )
+        );
         MockTokenWithPermit token = new MockTokenWithPermit();
         
         vm.stopBroadcast();
-        
-        console.log("MockTokenWithPermit deployed at:", address(token));
-        console.log("Name:", token.name());
-        console.log("Symbol:", token.symbol());
-        console.log("Decimals:", token.decimals());
-        console.log("Total Supply:", token.totalSupply());
-        console.log("Deployer Balance:", token.balanceOf(msg.sender));
-        console.log("Domain Separator:", vm.toString(token.DOMAIN_SEPARATOR()));
     }
 }
